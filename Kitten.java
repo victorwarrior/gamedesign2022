8d726ffa11592214f3584a768e87d8f749b28cb4
Viols22 <vbo00@hotmail.com>
2022-10-04 09:19:31 +0200

    Tilføjet Kitten fil

---
+++ Kitten.java
@@ -0,0 +1,13 @@
+public class Kitten {
+
+    public double cuteness;
+    public static int count = 0;
+
+    public Kitten(double cuteness){
+        this.cuteness = cuteness;
+        count++;
+    }
+
+
+
+}
