0248de6dfee412f9d3a97445d416e7fbcee5dcf5
Magn7559 <magnusandersen1234@gmail.com>
2022-10-25 10:23:48 +0200

    Platform blocker

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -125,7 +125,10 @@ public class PlayerController : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision) {
         if (collision.gameObject.CompareTag("Platform")) onGround = true;
+        if (collision.gameObject.CompareTag("edge")) onGround = false;
+
     }
+
 
     private void OnCollisionExit2D(Collision2D collision) {
         if (collision.gameObject.CompareTag("Platform")) onGround = false;
