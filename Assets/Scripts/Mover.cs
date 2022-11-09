752c28e756b1cbac6bf7d8b56829b5d9226756eb
Magn7559 <magnusandersen1234@gmail.com>
2022-11-09 15:22:14 +0100

    CirkleFormation

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -6,8 +6,13 @@ public class Mover : MonoBehaviour
 {
     public bool updateOn = false;
     public bool opOgNed = false;
+    public bool venstreOgHojre = false;
+    public bool CircleFormation = false;
     public float Seconds = 5;
     public float hastighed;
+
+    int cyklus = 0;
+
 
     void Start()
     {
@@ -17,8 +22,8 @@ public class Mover : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (opOgNed == true) 
-        { 
+        if (opOgNed == true)
+        {
             if (updateOn == true)
             {
                 up();
@@ -32,43 +37,73 @@ public class Mover : MonoBehaviour
 
         else
         {
-            if (updateOn == true)
+            if (venstreOgHojre == true)
+            {
+                if (updateOn == true)
+                {
+                    left();
+                }
+
+                if (updateOn == false)
+                {
+                    right();
+                }
+            }
+        }
+
+
+        if (CircleFormation == true)
+        {
+
+            if (cyklus == 0)
             {
                 left();
             }
 
-            if (updateOn == false)
+            if (cyklus == 1)
+            {
+                up();
+            }
+
+            if (cyklus == 2)
             {
                 right();
             }
+
+            if (cyklus == 3)
+            {
+                down();
+            }
+
+        }
+    }
+
+        void up()
+        {
+            transform.Translate(Vector2.up * hastighed * Time.deltaTime, Space.World);
         }
 
-    }
+        void down()
+        {
+            transform.Translate(Vector2.down * hastighed * Time.deltaTime, Space.World);
+        }
 
-    void up()
-    {
-        transform.Translate(Vector2.up * hastighed * Time.deltaTime, Space.World);
-    }
-    
-    void down()
-    {
-        transform.Translate(Vector2.down * hastighed * Time.deltaTime, Space.World);
-    }
+        void left()
+        {
+            transform.Translate(Vector2.left * hastighed * Time.deltaTime, Space.World);
+        }
 
-    void left()
-    {
-        transform.Translate(Vector2.left * hastighed * Time.deltaTime, Space.World);
-    }
+        void right()
+        {
+            transform.Translate(Vector2.right * hastighed * Time.deltaTime, Space.World);
+        }
 
-    void right()
-    {
-        transform.Translate(Vector2.right * hastighed * Time.deltaTime, Space.World);
-    }
+        IEnumerator updateOff()
+        {
+            yield return new WaitForSeconds(Seconds);
+            updateOn = !updateOn;
+            cyklus = (cyklus + 1) % 4;
+            StartCoroutine(updateOff());
+        }
+ }
 
-    IEnumerator updateOff()
-    {
-        yield return new WaitForSeconds(Seconds);
-        updateOn = !updateOn;
-        StartCoroutine(updateOff());    
-    }
-}
