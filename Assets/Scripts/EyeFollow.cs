11769718b36aef946fced95791d0e4e97caf2fa8
Magn7559 <magnusandersen1234@gmail.com>
2022-11-23 15:55:08 +0100

    eye

---
+++ Assets/Scripts/EyeFollow.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EyeFollow : MonoBehaviour
+{
+    public GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        eyeFollow();
+    }
+
+    void eyeFollow()
+    {
+        Vector3 playerPos = player.transform.position;
+
+        Vector2 direction = new Vector2(
+            playerPos.x - transform.position.x,
+            playerPos.y - transform.position.y);
+
+        transform.up = direction;
+    }
+
+}
