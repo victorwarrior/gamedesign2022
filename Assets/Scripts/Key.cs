31197ee097c54c98703ce779b3e7d23479b8e010
Magn7559 <magnusandersen1234@gmail.com>
2022-11-25 11:21:16 +0100

    keyscript named

--- Assets/Scripts/Key.cs
+++ Assets/Scripts/Key.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Collectable : MonoBehaviour
+public class Key : MonoBehaviour
 {
     //public ParticleSystem KeyDust;
 
