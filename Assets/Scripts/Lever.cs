3495332777f9409fadeabea7a190eba5a7572d25
Viols22 <vbo00@hotmail.com>
2022-12-13 13:33:18 +0100

    bn

--- Assets/Scripts/Lever.cs
+++ Assets/Scripts/Lever.cs
@@ -9,7 +9,7 @@ public class Lever : MonoBehaviour
 
     private void OnTriggerStay2D(Collider2D other)
     {
-        if (other.gameObject.CompareTag("Player"))
+        if (other.gameObject.CompareTag("Player")||other.gameObject.CompareTag("Stone"))
         {
             Door.gameObject.GetComponent<DoorToLever>().stopUp();
             onButton = true;
